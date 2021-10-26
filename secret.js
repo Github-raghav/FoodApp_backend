5c6b78705ae5fa9dcb96bc16ef3299bd6f9475f1
Github-raghav <80196589+Github-raghav@users.noreply.github.com>
2021-10-26 13:37:25 +0530

    Delete secret.js

--- secret.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    APP_PASSWORD:"2tOxTCgsOXH0JL2A",
-    DBLink:`mongodb+srv://admin${this.APP_PASSWORD}@cluster0.utwxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    JWT_KEY:"zdrxhuxglbstnvsr",
-    KEY_ID:"rzp_test_dgEjGwJIjUySPv",
-    KEY_SECRET:"9QucnatXsSxDsIQR5mFnCh2o"
-}
